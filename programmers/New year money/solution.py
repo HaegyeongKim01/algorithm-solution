95868ba9b932e1a655bf04b4a4a58bacaa80ae70
haegyeong <haegyeong.kim01@gmail.com>
2023-01-25 11:14:23 +0900

    New year money Modified code & Create solutionDescription

--- programmers/New year money/solution.py
+++ programmers/New year money/solution.py
@@ -1,14 +1,14 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-dup_arr = arr
+sorted_arr = sorted(arr)
 for a in arr:
     small_cnt, big_cnt = 0, 0
     
-    sorted(dup_arr)
-    for i in range(len(dup_arr)):
-        if dup_arr[i] < a :
+    for i in range(len(sorted_arr)):
+        if sorted_arr[i] < a :
             small_cnt += 1
-        elif dup_arr[i] > a:
+            
+        elif sorted_arr[i] > a:
             big_cnt += 1
     print(small_cnt, big_cnt )
